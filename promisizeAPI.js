2eb310ca79d213306da01c296cb33df32acfe71f
Erez Giladi <erez@crossrider.com>
2013-08-31 23:26:04 +0300

    - fix for old api

--- promisizeAPI.js
+++ promisizeAPI.js
@@ -4,26 +4,26 @@
 	// appAPI.db.async wrapper
 	//
 	///////////////////////////////////////////////////////////////
-	function newDbFunctionGenerator(oldDbFunction) {
-		return function () {
-			var dfd = new $.Deferred();
-			var args = Array.prototype.slice.call(arguments.length - 1);
-
-			// Save a ref to the callback if it was passed.
-			// Internally, API also uses these functions so we MUST call callback later
-			var callback = appAPI.utils.isFunction(arguments[arguments.length - 1]) ? arguments[arguments.length - 1] : noOpp;
-
-			args.push(function (result){
-				callback(result);
-				dfd.resolve(result);
-			});
-			// TODO - might need to call the function with setImmediate (to support IE)
-			oldDbFunction.apply(null, args);
-
-			return dfd.promise();
-		};
-	}
 	(function (db){
+		function newDbFunctionGenerator(oldDbFunction) {
+			return function () {
+				var dfd = new $.Deferred();
+
+				var args = Array.prototype.slice.call(arguments.length - 1);
+				// Save a ref to the callback if it was passed.
+				// Internally, API also uses these functions so we MUST call callback later
+				var callback = appAPI.utils.isFunction(arguments[arguments.length - 1]) ? arguments[arguments.length - 1] : noOpp;
+
+				args.push(function (result){
+					callback(result);
+					dfd.resolve(result);
+				});
+				// TODO - might need to call the function with setImmediate (to support IE)
+				oldDbFunction.apply(null, args);
+
+				return dfd.promise();
+			};
+		}
 		db.set = newDbFunctionGenerator(db.set);
 		db.get = newDbFunctionGenerator(db.get);
 		db.remove = newDbFunctionGenerator(db.remove);
@@ -56,19 +56,83 @@
 	// appAPI.request wrapper
 	//
 	///////////////////////////////////////////////////////////////
-	function newRequstFunctionGenerator(oldRequestFunction) {
-		return function (requestData) {
-			var dfd = new $.Deferred();
+	appAPI.request.get = function(urlToGet, onSuccess, onFailure) {
+		var dfd = new $.Deferred();
 
-			requestData.onSuccess = dfd.resolve;
-			requestData.onFailure = dfd.reject;
-			oldRequestFunction(requestData);
+		var oldGetRequestFunction = appAPI.request.get;
+		var requestObj = {};
+		if (appAPI.utils.isString(urlToGet)) {
+			// Its an old style request
+			requestObj.url = urlToGet;
+			onSuccess = onSuccess || noOpp;
+			onFailure = onFailure || noOpp;
+			requestObj.onSuccess = function (response, additionalInfo) {
+				onSuccess(response, additionalInfo);
+				dfd.resolve(response, additionalInfo);
+			};
+			requestObj.onFailure = function (httpCode){
+				onFailure(httpCode);
+				dfd.reject(httpCode);
+			};
+		}
+		else {
+			requestObj.url = urlToGet.url;
+			urlToGet.onSuccess = urlToGet.onSuccess || noOpp;
+			urlToGet.onFailure = urlToGet.onFailure || noOpp;
+			requestObj.onSuccess = function (response, additionalInfo) {
+				urlToGet.onSuccess(response, additionalInfo);
+				dfd.resolve(response, additionalInfo);
+			};
+			requestObj.onFailure = function (httpCode){
+				urlToGet.onFailure(httpCode);
+				dfd.reject(httpCode);
+			};
+			requestObj.additionalRequestHeaders = urlToGet.additionalRequestHeaders;
+		}
+		oldGetRequestFunction(requestObj);
 
-			return dfd.promise();
-		};
-	}
-	appAPI.appAPI.request.get = newRequstFunctionGenerator(appAPI.appAPI.request.get);
-	appAPI.appAPI.request.post = newRequstFunctionGenerator(appAPI.appAPI.request.post);
+		return dfd.promise();
+	};
+	appAPI.request.post = function(urlToPost, postData, onSuccess, onFailure) {
+		var dfd = new $.Deferred();
+
+		var oldPostRequestFunction = appAPI.request.post;
+		var requestObj = {};
+		if (appAPI.utils.isString(urlToPost)) {
+			// Its an old style request
+			onSuccess = onSuccess || noOpp;
+			onFailure = onFailure || noOpp;
+			requestObj.url = urlToPost;
+			requestObj.postData = postData;
+			requestObj.onSuccess = function (response, additionalInfo) {
+				onSuccess(response, additionalInfo);
+				dfd.resolve(response, additionalInfo);
+			};
+			requestObj.onFailure = function (httpCode){
+				onFailure(httpCode);
+				dfd.reject(httpCode);
+			};
+		}
+		else {
+			urlToPost.onSuccess = urlToPost.onSuccess || noOpp;
+			urlToPost.onFailure = urlToPost.onFailure || noOpp;
+			requestObj.url = urlToPost.url;
+			requestObj.postData = urlToPost.postData;
+			requestObj.onSuccess = function (response, additionalInfo) {
+				urlToPost.onSuccess(response, additionalInfo);
+				dfd.resolve(response, additionalInfo);
+			};
+			requestObj.onFailure = function (httpCode){
+				urlToPost.onFailure(httpCode);
+				dfd.reject(httpCode);
+			};
+			requestObj.additionalRequestHeaders = urlToGet.additionalRequestHeaders;
+			requestObj.contentType = urlToPost.contentType;
+		}
+		oldPostRequestFunction(requestObj);
+
+		return dfd.promise();
+    };
 }());
 
 
